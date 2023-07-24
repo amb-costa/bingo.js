263902e0a29c7d1656fe1f873929d7f6a8e999cb
elektra <orion@elektras-MacBook-Air.local>
2023-07-24 14:00:18 -0400

    first commit! trying to figure out how to generate the bingo card

---
+++ src/js/component/card.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="card bg-white border border-black">
+      <div className="card-body row">
+        <h3 className="col-12">B I N G O</h3>
+        <div className="col-8"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,17 @@
 import React from "react";
+import Card from "../../js/component/card.js";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
+  <div className="text-center mt-5 bg-success">
+    <h1>Cool bingo!</h1>
+    <Card />
+    <p>
+      <img src={rigoImage} />
+    </p>
+    <a href="#" className="btn btn-success">
+      If you see this green button, bootstrap is working
+    </a>
+  </div>
 );
